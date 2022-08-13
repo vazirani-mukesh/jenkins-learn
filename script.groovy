a2b982297914f7bcd16eafa6f929fe3d104dc121
Mukesh Vazirani <46274179+vazirani-mukesh@users.noreply.github.com>
2022-08-13 19:26:32 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,3 +1,19 @@
 def buildJar() {
-  sh 'mvn package'
+    sh 'mvn package'
 }
+
+def dockerBuild() {
+    sh "docker build -t ${params.DOCKER_USER_NAME}/myapp:${BUILD_NUMBER} ."
+}
+
+def dockerLogin() {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'MYPSW', usernameVariable: 'USER')]) {
+        sh "echo ${MYPSW} | docker login -u ${USER} --password-stdin"
+    }
+}
+
+def dockerPush() {
+    sh "docker push ${params.DOCKER_USER_NAME}/myapp:${BUILD_NUMBER}"
+}
+
+return this
